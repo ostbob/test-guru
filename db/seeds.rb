8686fd1e9445f9134b3f9dad69f385701e72e05e
ostbobmuon <ostbobmuon@gmail.com>
2021-04-08 14:55:56 +0300

    author added to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,11 @@ users = User.create([
 
 # Tests
 tests = Test.create([
-  { title: 'Test 1', level: 1, category_id: categories[0].id},
-  { title: 'Test 2', level: 2, category_id: categories[0].id},
-  { title: 'Test 3', level: 3, category_id: categories[1].id},
-  { title: 'Test 4', level: 3, category_id: categories[1].id},
-  { title: 'Test 5', level: 4, category_id: categories[2].id}
+  { title: 'Test 1', level: 1, category_id: categories[0].id, author_id: users[0].id},
+  { title: 'Test 2', level: 2, category_id: categories[0].id, author_id: users[1].id},
+  { title: 'Test 3', level: 3, category_id: categories[1].id, author_id: users[1].id},
+  { title: 'Test 4', level: 3, category_id: categories[1].id, author_id: users[0].id},
+  { title: 'Test 5', level: 4, category_id: categories[2].id, author_id: users[2].id}
 ])
 
 # TestPassages
