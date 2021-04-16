db1eb8b63528911be634767ef3337b19f0303147
ostbobmuon <ostbobmuon@gmail.com>
2021-04-16 19:10:35 +0300

    scaffold answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'tests#index'
+
   resources :tests do
-    resources :questions, shallow: true
-  end
+    resources :questions, shallow: true, except: :index do
+     resources :answers, shallow: true
+    end
+ end
 
 end
