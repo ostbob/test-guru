5d332c9b80e6e293ef4a06cda3a5a7bfc09df55a
ostbobmuon <ostbobmuon@gmail.com>
2021-04-24 17:56:49 +0300

    User auth and session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github_url(author, repo, tag_body)
     link_to tag_body, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(alert)
+    if flash[alert]
+      content_tag :p, flash[alert], class: 'flash alert'
+    else
+      ''
+    end
+  end
 end
