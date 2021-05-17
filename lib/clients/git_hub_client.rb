fd7294f6ab13d2b907beeeb60aab8d7dd47c3644
ostbobmuon <ostbobmuon@gmail.com>
2021-05-17 10:42:13 +0300

    fix codereview problems

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,7 +1,7 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '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'
+  ACCESS_TOKEN = ENV['ACCESS_TOKEN']
 
   def initialize
     @http_client = setup_http_client
