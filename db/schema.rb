c5845716e3495a4ce1e833839b7b3851d3daa98e
ostbobmuon <ostbobmuon@gmail.com>
2021-08-03 22:03:29 +0300

    Fix for db/seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_05_16_144357) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "title", null: false
     t.boolean "correct", default: false
-    t.integer "question_id"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 2021_05_16_144357) do
 
   create_table "gists", force: :cascade do |t|
     t.string "url", null: false
-    t.integer "question_id"
-    t.integer "user_id"
+    t.bigint "question_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_gists_on_question_id"
@@ -39,18 +42,18 @@ ActiveRecord::Schema.define(version: 2021_05_16_144357) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id"
+    t.bigint "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "test_passages", force: :cascade do |t|
-    t.integer "test_id"
-    t.integer "user_id"
+    t.bigint "test_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "current_question_id"
+    t.bigint "current_question_id"
     t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
@@ -60,10 +63,10 @@ ActiveRecord::Schema.define(version: 2021_05_16_144357) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-    t.integer "category_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
@@ -95,4 +98,13 @@ ActiveRecord::Schema.define(version: 2021_05_16_144357) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "gists", "questions"
+  add_foreign_key "gists", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Categories
-categories = Category.create([{ title: 'category 1'}, {title: 'category 2'}, {title: 'category 3'}])
+categories = Category.create!(
+  [{ title: 'Programming Language'},
+   {title: 'History'},
+   {title: 'Finance'}]
+)
 
 # Users
-users = User.create([
-  { email: 'bill@gmail.com', first_name: 'Bill', last_name: 'Murray'},
-  { email: 'mike@gmail.com', first_name: 'Mike', last_name: 'Tyson'},
-  { email: 'conan@gmail.com', first_name: 'Conan', last_name: 'McGregor'},
-  { email: 'habib@gmail.com', first_name: 'Habib', last_name: 'Nurmagomedov'}
+users = User.create!([
+  { email: 'bill@gmail.com', first_name: 'Bill', last_name: 'Murray', password: 'password'},
+  { email: 'mike@gmail.com', first_name: 'Mike', last_name: 'Tyson', password: 'password'},
+  { email: 'conan@gmail.com', first_name: 'Conan', last_name: 'McGregor', password: 'password'},
+  { email: 'habib@gmail.com', first_name: 'Habib', last_name: 'Nurmagomedov', password: 'password'}
 ])
 
 # Tests
-tests = Test.create([
-  { title: 'Test 1', level: 1, category_id: categories[0].id, author_id: users[0].id},
-  { title: 'Test 2', level: 2, category_id: categories[0].id, author_id: users[1].id},
-  { title: 'Test 3', level: 3, category_id: categories[1].id, author_id: users[1].id},
-  { title: 'Test 4', level: 3, category_id: categories[1].id, author_id: users[0].id},
-  { title: 'Test 5', level: 4, category_id: categories[2].id, author_id: users[2].id}
+tests = Test.create!([
+  { title: 'Ruby', level: 1, category_id: categories[0].id, author_id: 1},
+  { title: 'Java', level: 2, category_id: categories[0].id, author_id: 1},
+  { title: 'Russian History', level: 3, category_id: categories[1].id, author_id: 1},
+  { title: 'American Civil War', level: 3, category_id: categories[1].id, author_id: 1},
+  { title: 'Corporate Finance', level: 4, category_id: categories[2].id, author_id: 1}
 ])
 
 # TestPassages
-testPassages = TestPassage.create([
+testPassages = TestPassage.create!([
   { user_id: users[0].id, test_id: tests[0].id},
   { user_id: users[0].id, test_id: tests[1].id},
   { user_id: users[0].id, test_id: tests[2].id},
@@ -36,50 +40,58 @@ testPassages = TestPassage.create([
 ])
 
 # Questions
-questions = Question.create([
-  { body: 'Question 1', test_id: tests[0].id},
-  { body: 'Question 2', test_id: tests[0].id},
-  { body: 'Question 3', test_id: tests[0].id},
-  { body: 'Question 4', test_id: tests[1].id},
-  { body: 'Question 5', test_id: tests[1].id},
-  { body: 'Question 6', test_id: tests[1].id},
-  { body: 'Question 7', test_id: tests[2].id},
-  { body: 'Question 8', test_id: tests[2].id},
-  { body: 'Question 9', test_id: tests[3].id},
-  { body: 'Question 10', test_id: tests[4].id}
+questions = Question.create!([
+  { body: 'Is Ruby a strongly typed language?', test_id: tests[0].id},
+  { body: 'Ruby is an object oriented general purpose programming language.', test_id: tests[0].id},
+  { body: '...... syntax matches with the Ruby syntax.', test_id: tests[0].id},
+  { body: 'Which of the following is the file extension used for saving the ruby file?', test_id: tests[0].id},
+  { body: 'What are the sequence of ruby strings?', test_id: tests[0].id},
+  { body: 'Which of the following type of comments are valid in Ruby?', test_id: tests[0].id},
+  { body: 'What is Ruby?', test_id: tests[0].id},
+  { body: 'Ruby was developed by ......', test_id: tests[0].id},
+  { body: 'What of the following is the default value of a local variable?', test_id: tests[1].id},
+  { body: 'What is the size of double variable?', test_id: tests[1].id},
+  { body: 'What is the default value of short variable?', test_id: tests[1].id},
+  { body: 'What is class variable?', test_id: tests[1].id}
 ])
 
 # Answers
-answers = Answer.create([
-  { title: 'Answer 1', correct: false, question: questions[0]},
-  { title: 'Answer 2', correct: false, question: questions[0]},
-  { title: 'Answer 3', correct: true, question: questions[0]},
-  { title: 'Answer 4', correct: false, question: questions[1]},
-  { title: 'Answer 5', correct: false, question: questions[1]},
-  { title: 'Answer 6', correct: true, question: questions[1]},
-  { title: 'Answer 7', correct: false, question: questions[2]},
-  { title: 'Answer 8', correct: false, question: questions[2]},
-  { title: 'Answer 9', correct: true, question: questions[2]},
-  { title: 'Answer 10', correct: false, question: questions[3]},
-  { title: 'Answer 11', correct: false, question: questions[3]},
-  { title: 'Answer 12', correct: true, question: questions[3]},
-  { title: 'Answer 13', correct: false, question: questions[4]},
-  { title: 'Answer 14', correct: false, question: questions[4]},
-  { title: 'Answer 15', correct: true, question: questions[4]},
-  { title: 'Answer 16', correct: false, question: questions[5]},
-  { title: 'Answer 17', correct: false, question: questions[5]},
-  { title: 'Answer 18', correct: true, question: questions[5]},
-  { title: 'Answer 19', correct: false, question: questions[6]},
-  { title: 'Answer 20', correct: false, question: questions[6]},
-  { title: 'Answer 21', correct: true, question: questions[6]},
-  { title: 'Answer 22', correct: false, question: questions[7]},
-  { title: 'Answer 23', correct: false, question: questions[7]},
-  { title: 'Answer 24', correct: true, question: questions[7]},
-  { title: 'Answer 25', correct: false, question: questions[8]},
-  { title: 'Answer 26', correct: false, question: questions[8]},
-  { title: 'Answer 27', correct: true, question: questions[8]},
-  { title: 'Answer 28', correct: false, question: questions[9]},
-  { title: 'Answer 29', correct: false, question: questions[9]},
-  { title: 'Answer 30', correct: true, question: questions[9]}
+answers = Answer.create!([
+  { title: 'No', correct: false, question: questions[0]},
+  { title: 'Yes', correct: true, question: questions[0]},
+  { title: 'False', correct: false, question: questions[1]},
+  { title: 'True', correct: true, question: questions[1]},
+  { title: 'Java', correct: false, question: questions[2]},
+  { title: 'Python', correct: false, question: questions[2]},
+  { title: 'Perl', correct: true, question: questions[2]},
+  { title: '.ruby', correct: false, question: questions[3]},
+  { title: '.rrb', correct: false, question: questions[3]},
+  { title: '.rb', correct: true, question: questions[3]},
+  { title: '8-bit bytes', correct: true, question: questions[4]},
+  { title: '16-bit bytes', correct: false, question: questions[4]},
+  { title: '32-bit bytes', correct: false, question: questions[4]},
+  { title: 'Single Line', correct: false, question: questions[5]},
+  { title: 'Multiple Line', correct: false, question: questions[5]},
+  { title: 'Both Single & Multiple Line', correct: true, question: questions[5]},
+  { title: 'Markup language', correct: false, question: questions[6]},
+  { title: 'Procedural language', correct: false, question: questions[6]},
+  { title: 'Scripting language', correct: true, question: questions[6]},
+  { title: 'Vladimir Putin', correct: false, question: questions[7]},
+  { title: 'Brendan Eich', correct: false, question: questions[7]},
+  { title: 'Yukihiro Matsumoto', correct: true, question: questions[7]},
+  { title: 'null', correct: false, question: questions[8]},
+  { title: '0', correct: false, question: questions[8]},
+  { title: 'Not assigned', correct: true, question: questions[8]},
+  { title: '8 bit', correct: false, question: questions[9]},
+  { title: '16 bit', correct: false, question: questions[9]},
+  { title: '64 bit', correct: true, question: questions[9]},
+  { title: '0.0', correct: false, question: questions[10]},
+  { title: 'null', correct: false, question: questions[10]},
+  { title: 'not defined', correct: false, question: questions[10]},
+  { title: '0', correct: true, question: questions[10]},
+  { title: 'class variables are static variables within a class but outside any method.', correct: true, question: questions[11]},
+  { title: 'class variables are variables defined inside methods, constructors or blocks.', correct: false, question: questions[11]},
+  { title: 'class variables are variables within a class but outside any method.', correct: false, question: questions[11]},
+  { title: 'None of the above.', correct: false, question: questions[11]}
 ])
 
